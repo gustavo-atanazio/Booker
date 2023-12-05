1ad532bd6b9a900b839289d19928c9a708e87056
gustavo-atanazio <gus.atanazio@gmail.com>
2023-12-05 11:18:46 -0300

    Página Home (inicial)

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import Card from 'components/Card';
+import { useBooksContext } from 'context/Books';
+import styles from './Home.module.scss';
+
+function Home() {
+    const books = useBooksContext();
+
+    return (
+        <section className={styles.home}>
+            <h2>Meus livros</h2>
+
+            <div className={styles.home__container}>
+                {books.map(book => (
+                    <Card key={book.id} {...book}/>
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export default Home;
