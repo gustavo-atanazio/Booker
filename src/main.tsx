9bee60fc977f02f5646d22f4c9750d21c775d3e6
gustavo-atanazio <gus.atanazio@gmail.com>
2023-12-09 16:36:50 -0300

    Provendo contexto para a aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BooksProvider } from 'context/Books/index.tsx';
 import AppRouter from './routes.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <AppRouter/>
+        <BooksProvider>
+            <AppRouter/>
+        </BooksProvider>
     </React.StrictMode>,
 );
