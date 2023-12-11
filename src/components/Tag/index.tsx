9893d4272751230614d52a60aec38473c60f0102
gustavo-atanazio <gus.atanazio@gmail.com>
2023-12-11 16:21:27 -0300

    Corrigindo renderização

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -2,16 +2,17 @@ import styles from './Tag.module.scss';
 
 interface Props {
     text: string
-    color: string
+    backgroundColor: string
+    color?: string
     id?: string
     select?: (id: string) => void
     disabled?: boolean
 }
 
-function Tag({ text, color, id, select, disabled = false }: Props) {
+function Tag({ text, backgroundColor, color = '#FFF', id, select, disabled = false }: Props) {
     return (
         <button
-            style={{ backgroundColor: color }}
+            style={{ backgroundColor, color }}
             className={styles.tag} type='button'
             onClick={() => (id && select) && select(id)}
             disabled={disabled}
