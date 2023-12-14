a3a2330bc6d7133f1ca55c5d573006b535e26d37
gustavo-atanazio <gus.atanazio@gmail.com>
2023-12-14 10:10:46 -0300

    Função de deletar livro

--- src/context/Books/index.tsx
+++ src/context/Books/index.tsx
@@ -7,19 +7,39 @@ interface IBooksContext {
     books: IBook[]
     setBooks: React.Dispatch<React.SetStateAction<IBook[]>>
     createBook: (title: IBook['title'], author: IBook['author'], img: IBook['img'], tags: IBook['tags']) => void
+    deleteBook: (id: string) => void
     tags: ITag[]
 }
 
 const initialValue = {
     books: [{
         img: undefined,
-        title: 'Harry Potter',
+        title: 'Harry Potter e a Pedra Filosofal',
         author: 'J. K. Rowling',
-        tags: [tags[0]],
+        tags: [tags[0], tags[5], tags[1], tags[2]],
+        id: crypto.randomUUID()
+    }, {
+        img: undefined,
+        title: 'Harry Potter e a Pedra Filosofal',
+        author: 'J. K. Rowling',
+        tags: [tags[0], tags[5]],
+        id: crypto.randomUUID()
+    }, {
+        img: undefined,
+        title: 'Harry Potter e a Pedra Filosofal',
+        author: 'J. K. Rowling',
+        tags: [tags[0], tags[5]],
+        id: crypto.randomUUID()
+    }, {
+        img: undefined,
+        title: 'Harry Potter e a Pedra Filosofal',
+        author: 'J. K. Rowling',
+        tags: [tags[0], tags[5]],
         id: crypto.randomUUID()
     }],
     setBooks: () => {},
     createBook: () => {},
+    deleteBook: () => {},
     tags: []
 };
 
@@ -40,10 +60,16 @@ function BooksProvider({ children }: { children: React.ReactNode }) {
         setBooks(prevState => [...prevState, book]);
     }
 
+    function deleteBook(id: string) {
+        const updatedBooks = books.filter(item => item.id !== id);
+        setBooks(updatedBooks);
+    }
+
     const value = {
         books,
         setBooks,
         createBook,
+        deleteBook,
         tags
     };
 
