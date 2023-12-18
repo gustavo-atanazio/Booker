d8b34e56a3d1a1ae9289985e29b3bb9b1fa1a718
gustavo-atanazio <gus.atanazio@gmail.com>
2023-12-18 09:26:03 -0300

    Alterando index de estilos

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ModalProvider } from 'context/Modal/index.tsx';
 
 import AppRouter from 'routes';
 
-import './index.css';
+import 'styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
