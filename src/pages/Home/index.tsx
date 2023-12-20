a82c63ad5669cc773a493fb7241a2b843fa3601c
gustavo-atanazio <gus.atanazio@gmail.com>
2023-12-20 09:44:13 -0300

    Estruturando modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import Modal from 'react-modal';
 import Container from 'components/Container';
 import Card from 'components/Card';
 import Button from 'components/Button';
+import ModalForm from './ModalForm';
 
 import { useBooksContext } from 'context/Books';
 import { useModalContext } from 'context/Modal';
@@ -13,33 +14,38 @@ import styles from './Home.module.scss';
 function Home() {
     const navigate = useNavigate();
     const { books } = useBooksContext();
-    const { isOpen } = useModalContext();
+    const { isOpen, data } = useModalContext();
 
     return (
-        <section className={styles.home}>
-            <h2>Meus livros</h2>
+        <>
+            <section className={styles.home}>
+                <h2>Meus livros</h2>
 
-            <Container display='grid'>
-                {books.map(book => (
-                    <Card key={book.id} {...book}/>
-                ))}
-            </Container>
+                <Container display='grid'>
+                    {books.map(book => (
+                        <Card key={book.id} {...book}/>
+                    ))}
+                </Container>
 
-            <Button
-                type='button'
-                backgroundColor='#3D5A80'
-                color='#FFF'
-                width='50%'
-                fontSize={20}
-                onClick={() => navigate('/novo-livro')}
-            >
-                Criar novo
-            </Button>
+                <Button
+                    type='button'
+                    backgroundColor='#3D5A80'
+                    color='#FFF'
+                    width='50%'
+                    fontSize={20}
+                    onClick={() => navigate('/novo-livro')}
+                >
+                    Criar novo
+                </Button>
+            </section>
 
-            <Modal isOpen={isOpen}>
-                <p>Teste</p>
+            <Modal isOpen={isOpen} ariaHideApp={false}>
+                <h2>O que deseja editar?</h2>
+                <hr/>
+
+                <ModalForm bookTags={data.tags.map(tag => tag.id)} {...data}/>
             </Modal>
-        </section>
+        </>
     );
 }
 
