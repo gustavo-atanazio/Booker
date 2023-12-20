3b9d2fce891447c0f8a5021fda550a500bb82e7b
gustavo-atanazio <gus.atanazio@gmail.com>
2023-12-20 09:44:42 -0300

    Funções para abrir e fechar modal

--- src/context/Modal/index.tsx
+++ src/context/Modal/index.tsx
@@ -1,30 +1,49 @@
 import { createContext, useContext, useState } from 'react';
+import IBook from 'types/IBook';
 
 interface IModalContext {
     isOpen: boolean
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
-    showModal: () => void
+    data: IBook
+    showModal: (data: IBook) => void
+    closeModal: () => void
 }
 
 const initialValue = {
     isOpen: false,
     setIsOpen: () => {},
-    showModal: () => {}
+    data: {
+        img: undefined,
+        title: '',
+        author: '',
+        tags: [{ text: '', backgroundColor: '', id: '', selected: false }],
+        id: ''
+    },
+    showModal: () => {},
+    closeModal: () => {}
 };
 
 const ModalContext = createContext<IModalContext>(initialValue);
 
 function ModalProvider({ children }: { children: React.ReactNode }) {
     const [isOpen, setIsOpen] = useState(false);
+    const [data, setData] = useState<IBook>(initialValue.data);
 
-    function showModal() {
+    function showModal(data: IBook) {
         setIsOpen(true);
+        setData(data);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
     }
 
     const value = {
         isOpen,
         setIsOpen,
-        showModal
+        data,
+        showModal,
+        closeModal
     };
 
     return (
