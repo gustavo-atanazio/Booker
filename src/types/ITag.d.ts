e2398583a27333fa1f53acca74974b5110a741b6
Gustavo Atanazio <gus.atanazio@gmail.com>
2025-02-01 16:55:49 -0300

    chore: limpando o projeto

--- src/types/ITag.d.ts
+++
@@ -1,8 +0,0 @@
-interface ITag {
-    text: string
-    backgroundColor: string
-    color?: string
-    id: string
-}
-
-export default ITag;
--- src/utils/remove-script.ts
+++
@@ -1,5 +0,0 @@
-export function removeScript(string: string) {
-    const regex = /<script>[\s\S]*?<\/script>/;
-    string.replace(regex, '');
-    return string;
-}
